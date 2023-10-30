fbc6382cbdd2f7688fca42df66a77af624f49b9c
Biplab Sharma <jobs.biplab.sharma@gmail.com>
2023-10-31 01:47:33 +0530

    side menu v2

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-folder',
@@ -9,9 +9,13 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    if (this.folder === 'inbox')
+    {
+      this.router.navigateByUrl('inbox');
+    }
   }
 }
